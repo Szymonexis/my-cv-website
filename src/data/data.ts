7d8f506441072e3849180da7e6aca8704f9b4bcf
Szymonexis <szymonkaszuba3@wp.pl>
2022-10-25 22:32:33 +0200

    add nav and finnish welcome screen

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,3 +1,28 @@
 export const data = {
-	welcome: {},
+	aboutMe: {},
+	jobs: [{name: '', from: '', to: '', description: ''}],
+	languages: [
+		{ name: 'TypeScript', level: 95, graphic: 'typescript.gif' },
+		{ name: 'HTML & CSS', level: 95, graphic: 'html_css.gif' },
+		{ name: 'Python', level: 70, graphic: 'python.gif' },
+		{ name: 'C++', level: 60, graphic: 'cpp.gif' },
+		{ name: 'Java', level: 80, graphic: 'java.gif' },
+		{ name: 'Swift', level: 65, graphic: 'swift.gif' },
+		{ name: 'C#', level: 70, graphic: 'cs.gif' },
+		{ name: 'Rust', level: 40, graphic: 'rust.gif' },
+	],
+	technologies: [
+		{ name: 'Angular', level: 0, graphic: '' },
+		{ name: 'React', level: 0, graphic: '' },
+		{ name: 'React-Native', level: 0, graphic: '' },
+		{ name: 'NestJS', level: 0, graphic: '' },
+		{ name: 'Astro', level: 0, graphic: '' },
+		{ name: 'JavaFX', level: 0, graphic: '' },
+	],
+	hobbies: {},
+	links: {
+		github: 'https://github.com/Szymonexis',
+		linkedin:
+			'https://www.linkedin.com/in/szymon-kaszuba-ga%C5%82ka-394599200/',
+	},
 };
