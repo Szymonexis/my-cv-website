4860a319aa1ac365b48750e39e643c94eddc5ea3
Szymonexis <szymonkaszuba3@wp.pl>
2022-10-30 04:15:27 +0100

    update

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,6 +1,16 @@
 export const data = {
-	aboutMe: {},
-	jobs: [{name: '', from: '', to: '', description: ''}],
+	aboutMe: {
+		texts: [
+			'Hi there :)',
+			'I am Simon - your friendly neighbourhood Software Developer from Poland.',
+			'I started my journey with coding around {YEAR} years ago. I started coding because i found it very amusing and satisfying to make computer my work for me. Little later I started going to collage and discovered C++ and the beauty of algorithms, networks and website developement.',
+			'Now, I have finnished my batchelors with a grade of 4.8, managed to work within couple of bigger and smaller projects as a developer (both backend and frontend) and did some minor projects on of my own',
+			'My goal is to become a full-fledged proffessional - no code, no programming language, no task, hard or easy will stop me from making that goal ;)',
+		],
+		date: new Date(2016, 5),
+		picture: '/profile_picture.jpg',
+	},
+	jobs: [{ name: '', from: '', to: '', description: '' }],
 	languages: [
 		{ name: 'TypeScript', level: 95, graphic: 'typescript.gif' },
 		{ name: 'HTML & CSS', level: 95, graphic: 'html_css.gif' },
